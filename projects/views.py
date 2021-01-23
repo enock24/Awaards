ccb2df1c3ebbc719b13b7a306440807d4f2f53e6
enock24 <enookimuu@gmail.com>
2021-01-23 21:39:51 +0300

    add imports to views.py og projects

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from .models import *
+from .forms import *
+
+
+#rest 
 
 # Create your views here.
