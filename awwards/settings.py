2d89bd8fd5415a5f8a355480f27c8224726934ad
enock24 <enookimuu@gmail.com>
2021-01-23 23:53:38 +0300

    modified settings.py

--- awwards/settings.py
+++ awwards/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'bootstrap3',
+    'star_ratings',
+    'rest_framework.authtoken'
+    
 ]
 
 MIDDLEWARE = [
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
