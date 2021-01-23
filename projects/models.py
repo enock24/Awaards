ad74d70899df274c45287e27eb75e74e080d23ec
enock24 <enookimuu@gmail.com>
2021-01-23 21:58:16 +0300

    add imports to models.py

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django_countries.fields import CountryField
+from cloudinary.models import CloudinaryField
 # Create your models here.
